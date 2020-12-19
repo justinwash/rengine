c12aa346ce22a77203f73a18a0da5d57716f39fb
justin wash <justin.wash@pm.me>
2020-12-19 16:56:04 -0600

    basic texture rendering

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use glfw::{Action, Context as _, Key, WindowEvent};
-use luminance::context::GraphicsContext as _;
-use luminance::pipeline::PipelineState;
+use luminance::backend::texture::Texture as TextureBackend;
+use luminance::blending::{Blending, Equation, Factor};
+use luminance::context::GraphicsContext;
+use luminance::pipeline::{PipelineState, TextureBinding};
+use luminance::pixel::{NormRGB8UI, NormUnsigned};
 use luminance::render_state::RenderState;
+use luminance::shader::Uniform;
 use luminance::tess::Mode;
+use luminance::texture::{Dim2, GenMipmaps, Sampler, Texture};
+use luminance::UniformInterface;
 use luminance_derive::{Semantics, Vertex};
 use luminance_glfw::GlfwSurface;
 use luminance_windowing::{WindowDim, WindowOpt};
+use std::path::Path;
 use std::process::exit;
-use std::time::Instant;
+//use std::time::Instant;
 
 fn main() {
     // our graphics surface
@@ -30,82 +37,70 @@ fn main() {
     }
 }
 
-#[derive(Copy, Clone, Debug, Semantics)]
-pub enum VertexSemantics {
-    #[sem(name = "position", repr = "[f32; 2]", wrapper = "VertexPosition")]
-    Position,
-    #[sem(name = "color", repr = "[u8; 3]", wrapper = "VertexRGB")]
-    Color,
+#[derive(UniformInterface)]
+struct ShaderInterface {
+    tex: Uniform<TextureBinding<Dim2, NormUnsigned>>,
 }
 
-#[derive(Copy, Clone, Debug, Vertex)]
-#[vertex(sem = "VertexSemantics")]
-pub struct Vertex {
-    #[allow(dead_code)]
-    position: VertexPosition,
-
-    #[allow(dead_code)]
-    #[vertex(normalized = "true")]
-    color: VertexRGB,
-}
-
-const VERTICES: [Vertex; 3] = [
-    Vertex::new(
-        VertexPosition::new([-0.5, -0.5]),
-        VertexRGB::new([255, 0, 0]),
-    ),
-    Vertex::new(
-        VertexPosition::new([0.5, -0.5]),
-        VertexRGB::new([0, 255, 0]),
-    ),
-    Vertex::new(VertexPosition::new([0., 0.5]), VertexRGB::new([0, 0, 255])),
-];
-
-const VS_STR: &str = include_str!("vs.glsl");
-const FS_STR: &str = include_str!("fs.glsl");
+const TEX_VS: &str = include_str!("texture-vs.glsl");
+const TEX_FS: &str = include_str!("texture-fs.glsl");
 
 fn main_loop(mut surface: GlfwSurface) {
-    let start_t = Instant::now();
-    let back_buffer = surface.back_buffer().unwrap();
+    let mut back_buffer = surface.back_buffer().unwrap();
 
-    let triangle = surface
+    let img = read_image(Path::new("test_texture.png")).expect("error while reading image on disk");
+    let (width, height) = img.dimensions();
+    let mut tex = load_from_disk(&mut surface, img);
+
+    let mut tex_program = surface
+        .new_shader_program::<(), (), ShaderInterface>()
+        .from_strings(TEX_VS, None, None, TEX_FS)
+        .expect("program creation")
+        .ignore_warnings();
+    let tex_tess = surface
         .new_tess()
-        .set_vertices(&VERTICES[..])
-        .set_mode(Mode::Triangle)
+        .set_vertex_nb(4)
+        .set_mode(Mode::TriangleFan)
         .build()
         .unwrap();
-
-    let mut program = surface
-        .new_shader_program::<VertexSemantics, (), ()>()
-        .from_strings(VS_STR, None, None, FS_STR)
-        .unwrap()
-        .ignore_warnings();
+    let render_st = &RenderState::default().set_blending(Blending {
+        equation: Equation::Additive,
+        src: Factor::SrcAlpha,
+        dst: Factor::Zero,
+    });
+    let mut resize = false;
 
     'app: loop {
-        // handle events
         surface.window.glfw.poll_events();
         for (_, event) in surface.events_rx.try_iter() {
             match event {
                 WindowEvent::Close | WindowEvent::Key(Key::Escape, _, Action::Release, _) => {
                     break 'app
                 }
+                WindowEvent::FramebufferSize(..) => {
+                    resize = true;
+                }
                 _ => (),
             }
         }
 
-        let t = start_t.elapsed().as_millis() as f32 * 1e-3;
-        let color = [t.cos(), t.sin(), 0.5, 1.];
+        if resize {
+            back_buffer = surface.back_buffer().unwrap();
+            resize = false;
+        }
 
         let render = surface
             .new_pipeline_gate()
             .pipeline(
                 &back_buffer,
-                &PipelineState::default().set_clear_color(color),
-                |_, mut shd_gate| {
-                    shd_gate.shade(&mut program, |_, _, mut rdr_gate| {
-                        rdr_gate.render(&RenderState::default(), |mut tess_gate| {
-                            tess_gate.render(&triangle)
-                        })
+                &PipelineState::default(),
+                |pipeline, mut shd_gate| {
+                    let bound_tex = pipeline.bind_texture(&mut tex)?;
+
+                    shd_gate.shade(&mut tex_program, |mut iface, uni, mut rdr_gate| {
+                        iface.set(&uni.tex, bound_tex.binding());
+
+                        rdr_gate.render(render_st, |mut tess_gate| tess_gate.render(&tex_tess))
                     })
                 },
             )
@@ -117,4 +112,24 @@ fn main_loop(mut surface: GlfwSurface) {
             break 'app;
         }
     }
+}
+
+fn read_image(path: &Path) -> Option<image::RgbImage> {
+    image::open(path).map(|img| img.flipv().to_rgb8()).ok()
+}
+
+fn load_from_disk<B>(surface: &mut B, img: image::RgbImage) -> Texture<B::Backend, Dim2, NormRGB8UI>
+where
+    B: GraphicsContext,
+    B::Backend: TextureBackend<Dim2, NormRGB8UI>,
+{
+    let (width, height) = img.dimensions();
+    let texels = img.into_raw();
+
+    let mut tex = Texture::new(surface, [width, height], 0, Sampler::default())
+        .expect("luminance texture creation");
+
+    tex.upload_raw(GenMipmaps::No, &texels).unwrap();
+
+    tex
 }
