73761c24cb05edc9acab2b6b1a98c51cf2d00492
justin wash <justin.wash@pm.me>
2020-12-22 21:48:39 -0600

    renderer to house sub-renderers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use glfw::{Action, Context as _, Key, WindowEvent};
-use luminance::context::GraphicsContext;
-use luminance::pipeline::PipelineState;
 use luminance_glfw::GlfwSurface;
 use luminance_windowing::{WindowDim, WindowOpt};
 use std::process::exit;
 
 mod graphics;
-use crate::graphics::sprite;
 use crate::graphics::sprite::*;
+use crate::graphics::*;
 
 fn main() {
     let dim = WindowDim::Windowed {
@@ -30,37 +28,29 @@ fn main() {
 }
 
 fn main_loop(mut surface: GlfwSurface) {
-    let back_buffer = surface.back_buffer().unwrap();
+    let mut back_buffer = surface.back_buffer().unwrap();
+    let mut renderer = Renderer::new(&mut surface);
 
-    let mut sprites: Vec<Sprite> = Vec::new();
+    // sprites for testing
     let sprite = Sprite::new(
         "test_texture.png".to_string(),
         Position { x: 0, y: 0 },
         Size { w: 512, h: 512 },
     );
-    sprites.push(sprite);
+    renderer.sprites.push(sprite);
     let sprite = Sprite::new(
         "test_texture.png".to_string(),
         Position { x: 200, y: 200 },
         Size { w: 512, h: 1024 },
     );
-    sprites.push(sprite);
+    renderer.sprites.push(sprite);
     let sprite = Sprite::new(
         "test_texture.png".to_string(),
         Position { x: 700, y: 400 },
         Size { w: 600, h: 300 },
     );
-    sprites.push(sprite);
-
-    let mut sprite_renderer = SpriteRenderer::new(&mut surface);
-
-    for sprite in &sprites {
-        let sprite_cl = sprite.clone();
-        sprite_renderer.textures.insert(
-            sprite.id,
-            sprite::load_from_disk(&mut surface, sprite_cl.image),
-        );
-    }
+    renderer.sprites.push(sprite);
+    // sprites for testing
 
     'app: loop {
         surface.window.glfw.poll_events();
@@ -73,16 +63,7 @@ fn main_loop(mut surface: GlfwSurface) {
             }
         }
 
-        let render = surface
-            .new_pipeline_gate()
-            .pipeline(
-                &back_buffer,
-                &PipelineState::default().set_clear_color([0.0, 0.0, 0.0, 1.0]),
-                |pipeline, mut shading_gate| {
-                    sprite_renderer.render(&pipeline, &mut shading_gate, &mut sprites)
-                },
-            )
-            .assume();
+        let render = renderer.render(&mut surface, &mut back_buffer);
 
         if render.is_ok() {
             surface.window.swap_buffers();
