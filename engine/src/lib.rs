d2e4c07ad38be2e17f0696a713a00200c2e7b0b9
Justin Wash <justin.wash@pm.me>
2026-03-25 14:30:22 -0500

    rollback refactor

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,24 +1,19 @@
 pub mod app;
 pub mod math;
 
-
 pub mod input;
 
-
 pub mod canvas;
-pub mod text;
 pub mod renderer;
 pub mod renderer3d;
-
+pub mod text;
 
 pub mod world;
 
-
 pub mod assets;
 
-
+#[cfg(feature = "rollback")]
 pub mod netcode;
-
 
 pub use app::run;
 pub use app::{Engine, EngineConfig, Game};
@@ -28,32 +23,27 @@ pub use math::Rect;
 pub use math::TimeState;
 pub use renderer::{Camera2D, DrawParams, Frame, TextureId};
 
-
-pub use world::{aabb_overlap, iso_to_screen, screen_to_iso, TileDef, TileMap};
 pub use world::tilemap;
-
+pub use world::{aabb_overlap, iso_to_screen, screen_to_iso, TileDef, TileMap};
 
 pub use assets::pixelart;
 pub use assets::{Animation, SpriteSheet};
 
-
 pub use canvas::{screen_to_ndc, Canvas, CanvasVertex};
 pub use text::FontAtlas;
 
-
-pub use input::{GamepadState, GamepadSystem};
 pub use gilrs::Button as GamepadButton;
+pub use input::{GamepadState, GamepadSystem};
 
-
-pub use netcode::{run_rollback, RollbackConfig, RollbackGame, SessionMode};
-
+#[cfg(feature = "rollback")]
+pub use netcode::OnlineConfig;
+#[cfg(feature = "rollback")]
+pub use netcode::{fletcher64, RollbackConfig, RollbackSession, Rollbackable, SessionMode};
 
 pub use app::{run3d, Engine3D, Game3D};
 pub use renderer3d::{cube_mesh, floor_quad, wall_quad};
 pub use renderer3d::{Camera3D, DrawCmd3D, Frame3D, MeshId, Vertex3D};
 
-
 pub use glam::{Vec2, Vec3};
-
 
 pub use winit::keyboard::KeyCode;
